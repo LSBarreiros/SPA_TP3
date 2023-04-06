7671f90ba71b99d0e45d48739ecb9171af79d79c
Lbarreiros1980 <88776130+Lbarreiros1980@users.noreply.github.com>
2023-04-06 18:56:57 -0400

    new comit

---
+++ project-vue/src/App.vue
@@ -0,0 +1,113 @@
+<template>
+        <!-- Navigation-->
+      <header className="header">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light shadow fixed-top">
+            <div class="container px-4 px-lg-5">
+              <a class="navbar-brand .mr-3" href="#">UShop</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item pl-5">
+                          <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+                        </li>
+                        <li class="nav-item">
+                          <router-link class="nav-link" :to="{name: 'about'}">About</router-link>
+                        </li>
+                        <li class="nav-item">
+                          <router-link class="nav-link" to="/new-product">New Product</router-link>
+                        </li>
+                    </ul>
+                    <form class="d-flex">
+                        <button class="btn btn-outline-dark" type="button" @click="toggleSideBar">
+                            <i class="bi-cart-fill me-1"></i>
+                            Cart
+                            <span class="badge bg-dark text-white ms-1 rounded-pill"> {{ totalQuantity }}</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="navbar navbar-light bg-light navbar-expand-md header-bg py-5">
+            <div class="container px-4 my-2">
+                <div class="text-center text-white">
+                    <h1 class="display-4  text-center fw-bolder py-5">The best deals</h1>
+                </div>
+            </div>
+        </header>
+        <Router-view
+        :inventory = "inventory"
+        :addTo = "addToCart"
+        :addInv = "addInventory"
+        :removeInv = "removeInventory"
+        :remItem = "removeItem"
+        :updateInv = "updateInventory"
+        />
+        <Sidebar
+        v-if="showSideBar"
+        :toggle = "toggleSideBar"
+        :cart = "cart"
+        :inventory = "inventory"
+        :remove = "removeItem"
+        />
+      </header>
+</template>
+
+<script>
+import Sidebar from '@/components/SideBar.vue'
+// import inventory from '@/product.json'
+import ProductDataService from '@/services/ProductDataService'
+
+export default {
+  components: {
+    Sidebar
+  },
+  data: () => {
+    return {
+      showSideBar: false,
+      inventory: [],
+      cart: {}
+    }
+  },
+  methods: {
+    toggleSideBar () {
+      this.showSideBar = !this.showSideBar
+    },
+    addToCart (product, index) {
+      if (!this.cart[product]) this.cart[product] = 0
+      this.cart[product] += this.inventory[index].quantity
+      this.inventory[index].quantity = 0
+    },
+    removeItem (name) {
+      delete this.cart[name]
+    },
+    addInventory (product) {
+      this.inventory.push(product)
+    },
+    removeInventory (index) {
+      this.inventory.splice(index, 1)
+    },
+    updateInventory (index, data) {
+      this.inventory[index].name = data.name
+      this.inventory[index].photo = data.photo
+      this.inventory[index].price = data.price
+      this.inventory[index].description = data.description
+      this.inventory[index].type = data.type
+    }
+  },
+  computed: {
+    totalQuantity () {
+      return Object.values(this.cart).reduce((acc, curr) => {
+        return acc + curr
+      }, 0)
+    }
+  },
+  mounted () {
+    ProductDataService.getAll()
+      .then(response => {
+        this.inventory = response.data
+        console.log(response.data)
+      })
+  }
+}
+</script>
---
+++ server/project-vue/src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div className='container py-5'>
+    <h4>About app</h4>
+    <p>Version 0.2</p>
+  </div>
+</template>
